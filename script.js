4c068e2b638c862fcc339c41ea3773bdb7292162
Luis Hemar <50631922+luishemar@users.noreply.github.com>
2025-03-23 17:39:58 +0100

    Primera subida

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿// Inicializa el mapa
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Agrega una capa de mapa
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// Agrega un marcador
+var marker = L.marker([51.5, -0.09]).addTo(map);
+marker.bindPopup('<b>¡Hola mundo!</b><br>Estoy en Londres.').openPopup();
